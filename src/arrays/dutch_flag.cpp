6c143bd6cfc35819051b14d3066ce0cf09f11c12
shamse <ehsan.shams@gmail.com>
2021-02-03 10:36:34 -0800

    dutch flag - bug fix

--- src/arrays/dutch_flag.cpp
+++ src/arrays/dutch_flag.cpp
@@ -16,24 +16,28 @@ void dutch_flag(T* a, int n, int ip)
 	while (i < i1)
 	{
 		if (a[i] < pv)
-			swap(a, i, ++i0);
+			swap(a, i++, ++i0);
 		else if (a[i] > pv)
 			swap(a, i, --i1);
-
-		i++;
+		else
+			i++;
+		
 		display(a,n);
 	}
 }
 
 int main()
 {
-	int a[] {0,1,2,0,2,1,1};
+	// int a[] {0,1,2,0,2,1,1};
 
-	dutch_flag(a, 7, 2);
-	dutch_flag(a, 7, 1);
+	// dutch_flag(a, 7, 2);
+	// dutch_flag(a, 7, 1);
 
-	int b[] {-3,0,-1,-5,1,1,-5,-3,4,2};
+	// int b[] {-3,0,-1,-5,1,1,-5,-3,4,2};
 
-	dutch_flag(b, 10, 5);
-	dutch_flag(b, 10, 2);
+	// dutch_flag(b, 10, 5);
+	// dutch_flag(b, 10, 2);
+
+	int c[] {-1,0,2,-1,1,-1,0,2};
+	dutch_flag(c, 8, 1);
 }
